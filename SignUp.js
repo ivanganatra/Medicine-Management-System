a18327a94f4e88a6074eec5a02b623e4f09215f3
Ivan Ganatra <56468910+ivanganatra@users.noreply.github.com>
2021-05-08 09:42:08 +0530

    Delete SignUp.js

--- SignUp.js
+++
@@ -1,7 +0,0 @@
-function myfunction(){
-    document.getElementById("FirstName").innerHTML="FirstName";
-    document.getElementById("Sirname").innerHTML="Sirname";
-    document.getElementById("Email id").innerHTML="Email id";
-    document.getElementById("Password").innerHTML="Password";
-    document.getElementById("Confirm Password").innerHTML="Confirm Password";
-}
