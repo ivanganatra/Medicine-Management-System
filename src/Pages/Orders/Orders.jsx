e8a0fd5a064cc42c68ef7adcf5538b81af4f0aad
Rishab Goyal <rishabgoyal432@gmail.com>
2021-06-18 01:16:49 +0530

    Order Update

--- src/Pages/Orders/Orders.jsx
+++ src/Pages/Orders/Orders.jsx
@@ -1,48 +1,55 @@
 import React, { Component } from 'react';
 import "./Orders.css";
+import OrderImg from "../../assets/images/Orders/list3.jpeg";
 class Orders extends Component {
   state = {}
   render() {
     return (
       <div className="" style={{ paddingTop: "95px" }}>
-        <div className="container">
-          <div className="row p-3 pt-5">
-            <div className="col-12">
-              <h1 className="main-heading">Your Orders</h1>
-            <hr />
-            </div>
-          {/* </div>
+        <div className="container main-container">
+          <div className="row p-3">
+            <div className="col-12 col-md-8 px-0">
+              <div className="col-12">
+                <h1 className="main-heading">Your Orders</h1>
+                <hr />
+              </div>
+              {/* </div>
           <div className="row p-3 pt-5"> */}
-            <div className="col-12 pt-4">
-              <table className="table table-borderless">
-                <thead className="table-primary">
-                  <tr>
-                    <th scope="col">Order Id</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Booking Time</th>
-                    <th scope="col">Description</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                  </tr>
-                </tbody>
-              </table>
+              <div className="col-12 pt-4">
+                <table className="table table-borderless">
+                  <thead>
+                    <tr>
+                      <th scope="col" className="ps-md-3"><span>Order Id</span> </th>
+                      <th scope="col" className="ps-md-3"><span>Status</span></th>
+                      <th scope="col" className="ps-md-3"><span>Booking Time</span></th>
+                      <th scope="col" className="ps-md-3"><span>Description</span></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="ps-md-3">1</td>
+                      <td className="ps-md-3">Mark</td>
+                      <td className="ps-md-3">Otto</td>
+                      <td className="ps-md-3">@mdo</td>
+                    </tr>
+                    <tr>
+                      <td className="ps-md-3">2</td>
+                      <td className="ps-md-3">Jacob</td>
+                      <td className="ps-md-3">Thornton</td>
+                      <td className="ps-md-3">@fat</td>
+                    </tr>
+                    <tr>
+                      <td className="ps-md-3">3</td>
+                      <td className="ps-md-3">Larry the Bird</td>
+                      <td className="ps-md-3">Larry the Bird</td>
+                      <td className="ps-md-3">@twitter</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div className="col-md-4 d-none d-md-block px-0">
+              <img src={OrderImg} className="pl-5" alt="Not Found" width="100%"/>
             </div>
           </div>
         </div>
