c481bf85bbfd76f5df1a2e189eed265d72eb3849
jatin831 <jatinbajaj2001@gmail.com>
2021-06-27 11:54:39 +0530

    Orders Updated

--- src/Pages/Orders/Orders.jsx
+++ src/Pages/Orders/Orders.jsx
@@ -7,65 +7,68 @@ import OrderImg from "../../assets/images/Orders/list11.jpeg";
 class Orders extends Component {
   state = {
     orders: [
-      {
-        accepted_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
-        created_at: "June 20, 2021 5:08:34 PM UTC+5:30",
-        created_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
-        description: "I am in urgent need of these medicines. No nearby shops have these in stock. Please help !",
-        img_url: "https://firebasestorage.googleapis.com/v0/b/frosthack-a595f.appspot.com/o/images%2Fprescription2.jpg?alt=media&token=e74e5cde-7ef0-40ab-9d79-10ebe21b08dc",
-        status: "collected",
-        id: "01001",
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, nemo!",
-      },
-      {
-        accepted_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
-        created_at: "June 22, 2021 8:55:38 PM UTC+5:30",
-        created_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
-        description: "I am in urgent need of these medicines. No nearby shops have these in stock. Please help !",
-        img_url: "https://firebasestorage.googleapis.com/v0/b/frosthack-a595f.appspot.com/o/images%2Fprescription2.jpg?alt=media&token=e74e5cde-7ef0-40ab-9d79-10ebe21b08dc",
-        id: "01002",
-        status: "confirmed",
-      },
-      {
-        accepted_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
-        created_at: "June 26, 2021 7:06:04 PM UTC+5:30",
-        created_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
-        description: "I am in urgent need of these medicines. No nearby shops have these in stock. Please help !",
-        img_url: "https://firebasestorage.googleapis.com/v0/b/frosthack-a595f.appspot.com/o/images%2Fprescription2.jpg?alt=media&token=e74e5cde-7ef0-40ab-9d79-10ebe21b08dc",
-        id: "01003",
-        status: "pending",
-      },
-      {
-        accepted_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
-        created_at: "June 29, 2021 10:48:59 PM UTC+5:30",
-        created_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
-        description: "I am in urgent need of these medicines. No nearby shops have these in stock. Please help !",
-        img_url: "https://firebasestorage.googleapis.com/v0/b/frosthack-a595f.appspot.com/o/images%2Fprescription2.jpg?alt=media&token=e74e5cde-7ef0-40ab-9d79-10ebe21b08dc",
-        id: "01004",
-        status: "confirmed",
-      },
-      {
-        accepted_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
-        created_at: "June 24, 2021 11:00:01 PM UTC+5:30",
-        created_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
-        description: "I am in urgent need of these medicines. No nearby shops have these in stock. Please help !",
-        img_url: "https://firebasestorage.googleapis.com/v0/b/frosthack-a595f.appspot.com/o/images%2Fprescription2.jpg?alt=media&token=e74e5cde-7ef0-40ab-9d79-10ebe21b08dc",
-        id: "01005",
-        status: "cancelled",
-      },
+      // {
+      //   accepted_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
+      //   created_at: "June 20, 2021 5:08:34 PM UTC+5:30",
+      //   created_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
+      //   description: "I am in urgent need of these medicines. No nearby shops have these in stock. Please help !",
+      //   img_url: "https://firebasestorage.googleapis.com/v0/b/frosthack-a595f.appspot.com/o/images%2Fprescription2.jpg?alt=media&token=e74e5cde-7ef0-40ab-9d79-10ebe21b08dc",
+      //   status: "collected",
+      //   id: "01001",
+      //   desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, nemo!",
+      // },
+      // {
+      //   accepted_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
+      //   created_at: "June 22, 2021 8:55:38 PM UTC+5:30",
+      //   created_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
+      //   description: "I am in urgent need of these medicines. No nearby shops have these in stock. Please help !",
+      //   img_url: "https://firebasestorage.googleapis.com/v0/b/frosthack-a595f.appspot.com/o/images%2Fprescription2.jpg?alt=media&token=e74e5cde-7ef0-40ab-9d79-10ebe21b08dc",
+      //   id: "01002",
+      //   status: "confirmed",
+      // },
+      // {
+      //   accepted_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
+      //   created_at: "June 26, 2021 7:06:04 PM UTC+5:30",
+      //   created_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
+      //   description: "I am in urgent need of these medicines. No nearby shops have these in stock. Please help !",
+      //   img_url: "https://firebasestorage.googleapis.com/v0/b/frosthack-a595f.appspot.com/o/images%2Fprescription2.jpg?alt=media&token=e74e5cde-7ef0-40ab-9d79-10ebe21b08dc",
+      //   id: "01003",
+      //   status: "pending",
+      // },
+      // {
+      //   accepted_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
+      //   created_at: "June 29, 2021 10:48:59 PM UTC+5:30",
+      //   created_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
+      //   description: "I am in urgent need of these medicines. No nearby shops have these in stock. Please help !",
+      //   img_url: "https://firebasestorage.googleapis.com/v0/b/frosthack-a595f.appspot.com/o/images%2Fprescription2.jpg?alt=media&token=e74e5cde-7ef0-40ab-9d79-10ebe21b08dc",
+      //   id: "01004",
+      //   status: "confirmed",
+      // },
+      // {
+      //   accepted_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
+      //   created_at: "June 24, 2021 11:00:01 PM UTC+5:30",
+      //   created_by: "Jjaxv2ISvdeN5lqaqTNqJvWxdcp1",
+      //   description: "I am in urgent need of these medicines. No nearby shops have these in stock. Please help !",
+      //   img_url: "https://firebasestorage.googleapis.com/v0/b/frosthack-a595f.appspot.com/o/images%2Fprescription2.jpg?alt=media&token=e74e5cde-7ef0-40ab-9d79-10ebe21b08dc",
+      //   id: "01005",
+      //   status: "cancelled",
+      // },
     ]
   }
 
   componentDidMount() {
-    db.collection("orders").orderBy('created_at').get().then(snapshot => {
+    db.collection("orders").orderBy('created_at', "desc").get().then(snapshot => {
       let userOrders = [];
       snapshot.docs.forEach(doc => {
         if(doc.data().created_by === this.props.userId) {
-          userOrders.push(doc.data());
+          userOrders.push({
+            ...doc.data(),
+            id: doc.id
+          });
         }
       })
-      console.log(this.props);
       console.log(userOrders);
+      this.setState({orders: userOrders});
     })
   }
 
@@ -118,8 +121,8 @@ class Orders extends Component {
                         return (
                           <>
                             <tr key={index}>
-                              <th className="ps-md-3">#{order.id}</th>
-                              <td className="ps-md-3">{new Date(order.created_at).toLocaleString([], { year: 'numeric', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' })}</td>{/* <td className="ps-md-3">{new Date(order.created_at).toLocaleTimeString()}</td> */}
+                              <th className="ps-md-3">{order.id}</th>
+                              <td className="ps-md-3">{new Date(order.created_at.seconds * 1000).toLocaleString([], { year: 'numeric', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' })}</td>{/* <td className="ps-md-3">{new Date(order.created_at).toLocaleTimeString()}</td> */}
                               <td className="ps-md-3">
                                 <span className={"badge text-capitalize "+statusClass}>
                                   {order.status}
